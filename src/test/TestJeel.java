0ca66151e15d9467136e8395a1746cabc229683f
Jeel Patel <jeelpatel007@yahoo.com>
2016-01-17 19:35:12 -0500

    Update TestJeel.java

--- src/test/TestJeel.java
+++ src/test/TestJeel.java
@@ -1,3 +1,7 @@
+/*
+This is a Java based Selenium functional test of http://jeel-patel.weebly.com
+First all important modules related to the Class is imported and then the main class 'TestJeel' begins
+*/
 package test;
 
 import org.openqa.selenium.By;
@@ -9,21 +13,31 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class TestJeel {
 
     public static void main(String[] args) throws InterruptedException {
-        WebDriver driver = new FirefoxDriver();
-        driver.manage().window().maximize();
-        driver.get("http://jeel-patel.weebly.com");
-        Thread.sleep(2500);
 
+// Setting up the driver object for Firefox browser        
+        WebDriver driver = new FirefoxDriver();
+//Initializing the browser screen to maximum size for optimal visibility
+        driver.manage().window().maximize();
+        
+//Directing the browser to intended URL
+        driver.get("http://jeel-patel.weebly.com");
+//Hold on for 2.5 seconds before going to the next page        
+        Thread.sleep(2500);
+        
+//Clicking the Enter button to go to the next page. Triggered the button by its Class-Name
         driver.findElement(By.className("wsite-button-inner")).click();
         Thread.sleep(3000);
-
+        
+// Creating a JavaScript object which will enable Scrolling Down the page
         JavascriptExecutor jse = (JavascriptExecutor)driver;
         jse.executeScript("window.scrollBy(0,850)","");
         Thread.sleep(2000);
-
+        
+//Navigating to the next page by Clicking its button. Button is triggered by its Class-Name
         driver.findElement(By.className("wsite-button-inner")).click();
         Thread.sleep(3000);
 
+//Similar process of above is repeated for all other pages till the last 'contact' page
         JavascriptExecutor jsee = (JavascriptExecutor)driver;
         jsee.executeScript("window.scrollBy(0,850)","");
         Thread.sleep(2000);
@@ -31,6 +45,7 @@ public class TestJeel {
         driver.findElement(By.className("wsite-button-inner")).click();
         Thread.sleep(3000);
 
+//Filling out the relevant text-box fields of the contact form 
         driver.findElement(By.id("input-572455315222663644")).sendKeys("Jeel");
         Thread.sleep(1000);
 
@@ -47,9 +62,10 @@ public class TestJeel {
         driver.findElement(By.id("input-713380684465530085")).sendKeys("Test Complete!!!!!");
         Thread.sleep(1000);
 
+//Finally calling the Submit button by its Class-Name and click it to submit the form entry
         driver.findElement(By.className("wsite-button-inner")).click();
         Thread.sleep(6000);
-
+//After completion of all automaation operations, the drivers are closed to release the memory and close the application
         driver.quit();
     }
 }
